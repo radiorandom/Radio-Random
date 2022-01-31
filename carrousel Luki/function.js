5493bec0cb3c8606c4c83e50bfff358d4c16ba94
radiorandom <96266090+radiorandom@users.noreply.github.com>
2022-01-31 12:50:06 -0600

    Delete carrousel Luki directory

--- carrousel Luki/function.js
+++
@@ -1,8 +0,0 @@
-const hamburguer = document.querySelector(".icon");
-const navMenu = document.querySelector(".nav__ul");
-
-hamburguer.addEventListener("click", responsive);
-
-function responsive() {
-	navMenu.classList.toggle("active");
-}
